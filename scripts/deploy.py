d7591be3a33ba2f1082f1f728ec7eeef7432ae9a
benslamawael <120719706+benslamawael@users.noreply.github.com>
2022-12-22 11:19:06 +0100

    configuring ganache

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -4,9 +4,11 @@ from web3 import Web3
 def deploy_fund_me():
     account = accounts[0]
     print("deploying Mocks")
-    mock_aggregator = MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from" : account})
-    price_feed_address = mock_aggregator.address
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(18, Web3.toWei(2000, "ether"), {"from" : account})
     print("Mocks Deployed")
+    price_feed_address = MockV3Aggregator[-1].address
+    print("deploying to ganache")
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from" : account}
